72b9c8845c4a6d96d92bebfe20ad1eb3b76db51b
szt1999 <131294123+szt1999@users.noreply.github.com>
2023-09-01 14:42:10 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #echo 'src-git small https://github.com/shiyu1314/openwrt-packages' >>feeds.conf.default
